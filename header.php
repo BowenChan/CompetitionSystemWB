5c4e5807a79c8bf6682fb8074f876df7de594d3c
BowenChan <BowenmChan@gmail.com>
2015-01-15 15:11:02 -0800

    Working on Header

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<nav>
+    	<ul>
+        	<li> Home </li>
+        	<li> Set up Matches </li>
+            <li> Create a Tournament </li>
+            <li> Add Player </li>
+            <li> Check ranks </li>
+            
+        </ul>
+        
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Competition System</title>
+</head>
+
+<body>
+
+<div id = "header">
+	<?php include('header.php'); ?>
+</div>
+
+<div id = "body">
+
+
+</div>
+
+<div id = "footer">
+
+</div>
+</body>
+</html>
