de8499168cd120def2a159a4c3a27e2e80d86632
BowenChan <BowenmChan@gmail.com>
2015-01-15 16:47:15 -0800

    Match Setup
    starting to work on the match handling

--- matchcreate.php
+++ matchcreate.php
@@ -18,13 +18,22 @@
         
         <div id = "body">
         <p style = 'text-align:center;padding:10px 0;'>Which sport would you like to play</p>
-        <form method = "GET" style= "text-align:center; margin:0 auto">
+        <form method = "GET" style= "text-align:center; margin:0 auto" action = "matchhandle.php">
     		<select>
             	<?php
 					foreach ($sports as $x)
                     	print "<option value='" . $x . "' style = 'margin: 0 auto'>" . $x . "</option>";
                 ?>
           	</select>
+            <br><br>
+            <p> How many Courts would you like </p>
+            <br>
+            <select>
+            	<?php
+					for($i = 1; $i <= 10; $i++)
+						print "<option value='" . $i . "' style = 'margin: 0 auto'>" . $i . "</option>";
+				?>
+            </select>
             <br><br>
             <input type = "submit" value = " Submit "></input>
         </form>
